95a843e55add2135276727b70f774dbd3c6c974a
ligiaiv <ligia.venturott@gmail.com>
2019-12-17 19:54:01 -0300

    added database config

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
 import {Router} from "express";
+import User from './app/models/User';
 
 const routes = new Router();
 
 
-routes.get('/',(req,res)=>{
-    return res.json({message:'Hello Ligia!'});
+routes.get('/',async (req,res)=>{
+    console.log("OIEEEEE 1");
+
+    const user = await User.create({
+        name: 'Ligia Iunes',
+        email: 'ligiaeevee@hotmail.com',
+        password_hash : '123456',
+    });
+    console.log("OIEEEEE 2");
+
+    return res.json(user);
 });
 
 export default routes;
